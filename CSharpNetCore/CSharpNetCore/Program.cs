da75570ec18d07fe5bda975f94e831b88528dcda
onozaty <onozaty@gmail.com>
2021-12-24 08:47:41 +0900

    [CSharp .NET Core] プロジェクトのベースを追加

---
+++ CSharpNetCore/CSharpNetCore/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+
+Console.WriteLine($"Version: {assembly.GetName().Version}");
+
+var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+Console.WriteLine($"FileVersion: {versionInfo.FileVersion}");
+Console.WriteLine($"ProductVersion: {versionInfo.ProductVersion}");
