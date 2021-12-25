2554ece5d920dc5b737209ef15d8c07ef835c160
onozaty <onozaty@gmail.com>
2021-12-25 21:41:04 +0900

    [CSharp .NET Framework] GitInfo版を追加

---
+++ CSharp_GitInfo/CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            Console.WriteLine($"Version: {assembly.GetName().Version}");
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            Console.WriteLine($"FileVersion: {versionInfo.FileVersion}");
+            Console.WriteLine($"ProductVersion: {versionInfo.ProductVersion}");
+        }
+    }
+}
---
+++ CSharp_GitInfo/CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Embed Version Sample")]
+[assembly: AssemblyDescription("Embed Version Sample Description")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Embed Version Sample")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+// https://github.com/devlooped/GitInfo
+[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+[assembly: AssemblyFileVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+[assembly: AssemblyInformationalVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch + ThisAssembly.Git.SemVer.DashLabel + "." + ThisAssembly.Git.Commit)]
+[assembly: Guid("ec86a331-8ca3-4979-a09b-e96995eac5b9")]
